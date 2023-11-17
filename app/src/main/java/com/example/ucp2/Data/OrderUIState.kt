96af612324175fc3075d0b1bf540f0ae4e89363d
Devina02 <devinafitriapngs@gmail.com>
2023-11-17 17:43:14 +0700

    first commit

---
+++ app/src/main/java/com/example/ucp2/Data/OrderUIState.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2.Data
+
+data class OrderUIState(
+    var nama: String= "",
+    var nim: String= "",
+    var konsentrasi: String= "",
+    var judulskripsi: String= "",
+    var dosenpem1: String= "",
+    var dospem2: String= ""
+)
---
+++ app/src/main/java/com/example/ucp2/Data/Sumber.kt
@@ -0,0 +1,11 @@
+package com.example.ucp2.Data
+
+import com.example.ucp2.R
+
+object Sumber {
+    val dosenpem1 = listOf(
+        R.string.dosenharis,
+        R.string.dosencahya,
+        R.string.desenaprilia,
+    )
+}
